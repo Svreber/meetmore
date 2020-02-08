86b957568387fa82bbdb813ccb8109505315cb2c
Lukino <LukinoProg@gmail.com>
2020-02-08 15:19:51 +0100

    Add allow-access-origin headers

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,19 @@
-import { Module } from '@nestjs/common';
+import {MiddlewareConsumer, Module} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {EventModule} from './modules/event/event.module';
+import {responseHeaders} from './core/middleware/response-header.middleware';
 
 @Module({
   imports: [EventModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer
+      .apply(responseHeaders)
+      .forRoutes('*');
+  }
+}
+
